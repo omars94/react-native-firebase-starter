f32fb8018924b298c91a7e2c6b21aad5bc36757d
Omar Samman <me.omar94@gmail.com>
2018-04-13 15:18:37 +0300

    start integrating native base

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Button, Image } from "react-native";
+import { StyleSheet, Image } from "react-native";
 import firebase from "react-native-firebase";
+import { Container, Button, Content, Header, Text } from "native-base";
 class settings extends Component {
 	render() {
 		return (
-			<View style={{ flex: 1 }}>
-				<Button
-					title="Logout"
-					onPress={() => {
-						firebase.auth().signOut();
-					}}
-				/>
-				<Text style={{ fontSize: 50, color: "red", textAlign: "center" }}>
-					This is the settings page
-				</Text>
-			</View>
+			<Container>
+				<Content>
+					<Button
+						onPress={() => {
+							firebase.auth().signOut();
+						}}
+					>
+						<Text>Logout</Text>
+					</Button>
+					<Text
+						style={{
+							fontSize: 50,
+							color: "red",
+							textAlign: "center"
+						}}
+					>
+						This is the settings page
+					</Text>
+				</Content>
+			</Container>
 		);
 	}
 }
