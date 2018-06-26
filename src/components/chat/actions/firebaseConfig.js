5fd45d1272683d5e43efd84c91a7d611aa45dc5b
Omar Samman <me.omar94@gmail.com>
2018-06-26 14:13:39 +0300

    working after error react native module

--- src/components/chat/actions/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDGbC1e0_KKjJU6mbfZJID__T5pC2-lkMY",
-  authDomain: "<PROJECT_ID>.firebaseapp.com",
-  databaseURL: "https://infinite-mantis-140010.firebaseio.com/",
-  projectId: "infinite-mantis-140010",
-  storageBucket: "infinite-mantis-140010.appspot.com",
-  messagingSenderId: "<SENDER_ID>",
-}
